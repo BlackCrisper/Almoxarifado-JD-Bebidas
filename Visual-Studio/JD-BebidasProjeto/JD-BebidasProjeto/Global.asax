e9c32a2db90d828d82cfac46d84b1ecd6cd51c07
nycolas reis <nycolasreis3@hotmail.com>
2022-09-22 14:45:09 -0300

    Pasta com os primeiros arquivos do visual

---
+++ Visual-Studio/JD-BebidasProjeto/JD-BebidasProjeto/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JD_BebidasProjeto.MvcApplication" Language="C#" %>
---
+++ Visual-Studio/JD-BebidasProjeto/JD-BebidasProjeto/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas através do seguinte
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("JD_BebidasProjeto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JD_BebidasProjeto")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como falso torna não visíveis os tipos neste assembly
+// para componentes COM. Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("e724aea8-3d4b-4758-8e60-8e3650b0f1c5")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Major Version
+//      Versão Secundária
+//      Build Number
+//      Revision
+//
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

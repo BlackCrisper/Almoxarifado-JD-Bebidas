821d46277a0ca471e222c807a21c7b134306c043
nycolas reis <nycolasreis3@hotmail.com>
2022-09-27 16:45:01 -0300

    Adicionando dados

--- Banco-de-dados/Inserts.sql
+++ Banco-de-dados/Inserts.sql
@@ -1,31 +1,36 @@
 
 
---Inserindo dados em Categoria
+	--Inserindo dados em Categoria
 
-insert CATEGORIA values ('Refrigerante')
-insert CATEGORIA values ('Cerveja')
-insert CATEGORIA values ('agua')
-
-
---Inserindo dados em Produtos
-
-insert PRODUTO(CATID,PRODESCRICAO) values (1,'Coca-Cola')
-insert PRODUTO(CATID,PRODESCRICAO) values (1,'Guarana')
-insert PRODUTO(CATID,PRODESCRICAO) values (2,'Skol')
-insert PRODUTO(CATID,PRODESCRICAO) values (2,'Bramha')
-insert PRODUTO(CATID,PRODESCRICAO) values (3,'Crystal')
-insert PRODUTO(CATID,PRODESCRICAO) values (3,'Lev')
+	insert CATEGORIA values ('Refrigerante')
+	insert CATEGORIA values ('Cerveja')
+	insert CATEGORIA values ('agua')
 
 
---Inserindo dados em Tipo
+	--Inserindo dados em Produtos
 
-insert TIPO values ('1 Litro')
-insert TIPO values ('2 Litros')
-insert TIPO values ('Lata')
-insert TIPO values ('500 ML')
+	insert PRODUTO(CATID,PRODESCRICAO) values (1,'Coca-Cola')
+	insert PRODUTO(CATID,PRODESCRICAO) values (1,'Guarana')
+	insert PRODUTO(CATID,PRODESCRICAO) values (2,'Skol')
+	insert PRODUTO(CATID,PRODESCRICAO) values (2,'Bramha')
+	insert PRODUTO(CATID,PRODESCRICAO) values (3,'Crystal')
+	insert PRODUTO(CATID,PRODESCRICAO) values (3,'Lev')
 
 
+	--Inserindo dados em Tipo
+
+	insert TIPO values ('1 Litro')
+	insert TIPO values ('2 Litros')
+	insert TIPO values ('Lata')
+	insert TIPO values ('500 ML')
 
 
+	--Inserindo dados em Entrada
 
+	insert ENTRADA values (02/20/22,'AMBEV','tudo ok')
+
+
+	--Inserindo dados em Saida
+
+	insert SAIDA values (02/20/22)
 

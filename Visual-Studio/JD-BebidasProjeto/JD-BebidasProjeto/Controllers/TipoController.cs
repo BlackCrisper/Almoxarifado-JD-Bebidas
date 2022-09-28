9d621808c2308f05818d2972394c276fb39bc2f7
nycolas reis <nycolasreis3@hotmail.com>
2022-09-28 14:46:38 -0300

    Atualizando crud de produtos add imagem

--- Visual-Studio/JD-BebidasProjeto/JD-BebidasProjeto/Controllers/TipoController.cs
+++ Visual-Studio/JD-BebidasProjeto/JD-BebidasProjeto/Controllers/TipoController.cs
@@ -2,14 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace JD_BebidasProjeto.Controllers
 {
-    public class ProdutoController : Controller
+    public class TipoController : Controller
     {
 
         BDJDBEBIDAS bd = new BDJDBEBIDAS();
@@ -20,12 +19,11 @@ namespace JD_BebidasProjeto.Controllers
         public ActionResult Index()
         {
             // select * from produto
-            return View(bd.PRODUTO.ToList());
+            return View(bd.TIPO.ToList());
         }
         // GET: Criar
         public ActionResult Criar()
         {
-            ViewBag.listaCategoria = bd.CATEGORIA.ToList();
             return View();
         }
         public ActionResult Erro()
@@ -34,19 +32,14 @@ namespace JD_BebidasProjeto.Controllers
         }
 
         [HttpPost]
-        public ActionResult Criar(string descricao,int categoria, HttpPostedFileBase imagem)
+        public ActionResult Criar(string descricao)
         {
-            PRODUTO novoproduto = new PRODUTO();
+            TIPO novotipo = new TIPO();
 
-            novoproduto.PRODESCRICAO = descricao;
-            novoproduto.CATID = categoria;
-            using (var memoryStream = new MemoryStream())
-            {
-                imagem.InputStream.CopyTo(memoryStream);
-                novoproduto.PROIMAGEM = memoryStream.ToArray();
-            }
+            novotipo.TIPNOME = descricao;
 
-            bd.PRODUTO.Add(novoproduto);
+
+            bd.TIPO.Add(novotipo);
             bd.SaveChanges();
 
             return RedirectToAction("Index");
@@ -57,18 +50,18 @@ namespace JD_BebidasProjeto.Controllers
         [HttpGet]
         public ActionResult Editar(int? id)
         {
-            PRODUTO prolocalizar = bd.PRODUTO.ToList().Where(x => x.PROID == id).First();
-            return View(prolocalizar);
+            TIPO tipolocalizar = bd.TIPO.ToList().Where(x => x.TIPID == id).First();
+            return View(tipolocalizar);
         }
 
         [HttpPost]
         public ActionResult Editar(int? id, string descricao)
         {
-            PRODUTO proatualizar = bd.PRODUTO.ToList().Where(x => x.PROID == id).First();
-            proatualizar.PRODESCRICAO = descricao;
+            TIPO tipoatualizar = bd.TIPO.ToList().Where(x => x.TIPID == id).First();
+            tipoatualizar.TIPNOME = descricao;
 
 
-            bd.Entry(proatualizar).State = EntityState.Modified;
+            bd.Entry(tipoatualizar).State = EntityState.Modified;
 
             bd.SaveChanges();
             return RedirectToAction("Index");
@@ -79,15 +72,15 @@ namespace JD_BebidasProjeto.Controllers
         [HttpGet]
         public ActionResult Deletar(int? id)
         {
-            PRODUTO proDeletar = bd.PRODUTO.ToList().Where(x => x.PROID == id).First();
-            return View(proDeletar);
+            TIPO tipoDeletar = bd.TIPO.ToList().Where(x => x.TIPID == id).First();
+            return View(tipoDeletar);
         }
 
         [HttpPost]
         public ActionResult ConfirmeDelete(int? id)
         {
-            PRODUTO proDeletar = bd.PRODUTO.ToList().Where(x => x.PROID == id).First();
-            bd.PRODUTO.Remove(proDeletar);
+            TIPO tipoDeletar = bd.TIPO.ToList().Where(x => x.TIPID == id).First();
+            bd.TIPO.Remove(tipoDeletar);
             try
             {
                 bd.SaveChanges();
@@ -103,6 +96,8 @@ namespace JD_BebidasProjeto.Controllers
 
             return RedirectToAction("Index");
         }
+
+      
 
 
 
